45aed474625a013c41b0cf94de1e974abf2b4d40
Reefersleep <soerenknud@gmail.com>
2025-06-06 23:20:14 +0200

    Update Clojure dependency

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/Reefersleep/thread-until"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.12.1"]]
   :repl-options {:init-ns thread-until.core})
